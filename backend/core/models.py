26f5a04affe057a3ba824682411e48af3ee5ad22
mioli <mateusmirandammo@gmail.com>
2025-11-12 13:58:30 -0300

    WIP: start models creation

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=150)
+    # ...
+
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=150)
+    # ...
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    # ...
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=150)
+    # ...
+
+class Movimentacao(models.Model):
+    valor_total = models.DecimalField(max_digits=7, decimal_places=2)
+    # ...
+
+class Produto_movimentacao(models.Model):
+    valor_unitario = models.DecimalField(max_digits=7, decimal_places=2)
+    # ...
